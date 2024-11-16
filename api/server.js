b2213274947b08c1c20815402a8b3b4aa7122bd1
Azeem Brown <azeembrown717@gmail.com>
2024-11-15 20:53:35 -0500

    FIXED

--- api/server.js
+++ api/server.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const restrict = require('./middleware/restricted.js');
-console.log(require('connect-session-knex'))
+
 const authRouter = require('./auth/auth-router.js');
 const jokesRouter = require('./jokes/jokes-router.js');
 const session = require("express-session")
-const Store = require('connect-session-knex')(session) //why is this giving me an error
+const { ConnectSessionKnexStore } = require("connect-session-knex"); //why is this giving me an error
 const server = express();
 
 server.use(helmet());
@@ -15,7 +15,7 @@ server.use(cors());
 server.use(express.json());
 server.use(session({
     name: "Goku",
-    secret: "Keep on the low",
+    secret: "Keeponthelow",
     cookie: {
         maxAge: 100 * 60 * 60,
         secure: false, //Set to true on live server
@@ -24,7 +24,7 @@ server.use(session({
     rolling: true, //Fresh cookie on every login
     resave: false,
     saveUninitialized: false, //This means we cannot set cookies on any client that makes a request
-    store: new Store({
+    store: new ConnectSessionKnexStore({
         knex: require('../data/dbConfig.js'),
         tablename: 'sessions',
         sidfieldname: 'sid',
